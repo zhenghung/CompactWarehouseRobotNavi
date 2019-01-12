36b5fa9c8016bcbb8e2212d6be0d3a0d25411df9
Bryan <bryanhzj@gmail.com>
2019-01-12 21:16:57 +0000

    interrupt

--- test/test.ino
+++ test/test.ino
@@ -1,6 +1,6 @@
-#define LeftHall 2
-#define RightHall 3
-#define pi 3.14159
+//#define LeftHall 2
+//#define RightHall 3
+#define pi 3.1416
 
 
 int val;  // used to store hall sensor values
@@ -15,30 +15,38 @@ float L = 69.16;  // distance travelled in one hall sensor reading period
 
 const int LeftHall = 2;
 const int RightHall = 3;
+const float HalfWidth = 300;
 
 volatile unsigned long LeftHallCount = 0;
 volatile unsigned long RightHallCount = 0;
-volatile int x = 0;
-volatile int y = 0;
+volatile float x = 0;
+volatile float y = 0;
 volatile float bearing = 0;
+volatile float new_bearing = 0;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   
   //set up pins
-  pinMode (outputPin, OUTPUT);
-  pinMode (MotorSpeed, OUTPUT);
+//  pinMode (outputPin, OUTPUT);
+//  pinMode (MotorSpeed, OUTPUT);
   pinMode (LeftHall, INPUT_PULLUP);
-  pinMode (RightHall, INPUT_PULLUP);
+  pinMode (RightHall, INPUT_PULLUP); 
+  pinMode (5, OUTPUT);
 
   // set up interrupts
-  attachInterrupt(digitalPinToInterrupt(LeftHall), CountLeft, FALLING);
-  attachInterrupt(digitalPinToInterrupt(RightHall), CountRight, FALLING);
+  attachInterrupt(digitalPinToInterrupt(LeftHall), CountLeft, RISING);
+  attachInterrupt(digitalPinToInterrupt(RightHall), CountRight, RISING);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+//  // put your main code here, to run repeatedly:
+//  digitalWrite(5, HIGH);
+////  Serial.println("HIGH");
+//  delay(1000);
+//  digitalWrite(5, LOW);
+////  Serial.println("LOW");
+//  delay(1000);
 }
 
 void CountLeft() {
@@ -53,10 +61,22 @@ void CountLeft() {
 //    x = x + HalfWidth*(1 - cos(bearing));
 //    y = y - HalfWidth*(tan(bearing) - sin(bearing));
 //  }
-    print(LeftHallCount)
-    bearing = bearing + (90*L)/(pi*HalfWidth);
-    x = x + HalfWidth*(1 - cos(bearing));
-    y = y + HalfWidth*(tan(bearing) - sin(bearing));
+    Serial.println(LeftHallCount);
+    new_bearing = bearing + (90*L)/(180*HalfWidth); // in radian
+    if (new_bearing >= 6.283) {
+      new_bearing = new_bearing - 6.283;
+    }
+    Serial.print("Bearing: ");
+    Serial.println(new_bearing);
+    x = x + HalfWidth*(cos(bearing) - cos(new_bearing));
+    
+    Serial.print("x coordinate: ");
+    Serial.println(x);
+    y = y + HalfWidth*(sin(new_bearing) - sin(bearing));
+    
+    Serial.print("y coordinate: ");
+    Serial.println(y);
+    bearing = new_bearing;
 }
 
 void CountRight() {
@@ -71,7 +91,10 @@ void CountRight() {
 //    x = x - HalfWidth*(1 - cos(bearing));
 //    y = y - HalfWidth*(tan(bearing) - sin(bearing));
 //  }
+    Serial.println(RightHallCount);
     bearing = bearing - (90*L)/(pi*HalfWidth);
+    
     x = x - HalfWidth*(1 - cos(bearing));
+    
     y = y + HalfWidth*(tan(bearing) - sin(bearing));
 }
