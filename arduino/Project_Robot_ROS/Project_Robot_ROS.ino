a79afb31ed387c40f6c44546e24ff0adb9b406e0
Zheng Hung Chuah <zheng_hung@hotmail.com>
2019-01-20 21:19:31 +0000

    Prototype ROS integrated Robot Control with Odom

--- arduino/Project_Robot_ROS/Project_Robot_ROS.ino
+++ arduino/Project_Robot_ROS/Project_Robot_ROS.ino
@@ -1,9 +1,180 @@
+// DEFINE CONSTANTS
+// PINS
+#define PWM_MOVE 5
+#define LEFT_HALL_IN A0
+#define RIGHT_HALL_IN A1
+#define LEFT_REVERSE 8
+#define RIGHT_REVERSE 9
+
+// CONSTANTS
+#define LEFT_HALL_THRESH 100
+#define RIGHT_HALL_THRESH 100
+#define DUTY_BEGIN 100
+
+// PHYSICS CONSTANTS
+#define WHEEL_CIRCUMFERENCE 518.36
+#define ROBOT_HALF_WIDTH 280
+
 // ROS Package and std_msg format
 #include <ros.h>
-#include <std_msgs/Int32.h>
+#include "geometry_msgs/Twist.h"
 
+//=======================================================
+// Globals
+ros::NodeHandle robot;
+
+// Rising Edge
+bool left_wasLowLevel = false;
+int left_hall_val;
+bool right_wasLowLevel=false;
+int right_hall_val;
+
+// Odometry
+unsigned long leftHallCount = 0;
+unsigned long rightHallCount = 0;
+volatile float x = 0;
+volatile float y = 0;
+volatile float theta = 0;
+
+// Functions
+void moveForward();
+void moveTurn(float angle);
+void moveStop();
+void velCallback( const geometry_msgs::Twist& vel);
+void pollHallPins();
+void updateOdom(int turn);
+
+// ROS Subscriber and Publisher
+ros::Subscriber<geometry_msgs::Twist> sub_cmd_vel("cmd_vel" , velCallback);
+ros::Publisher pub_odom("odom", &pose);
+
+//=======================================================
+// ROBOT MOVEMENT
+void moveForward(){
+  analogWrite(PWM_MOVE, DUTY_BEGIN);
+}
+
+void moveTurn(float angle){
+  if (angle>0){
+    //Turn Left
+    digitalWrite(LEFT_REVERSE, HIGH);
+    analogWrite(PWM_MOVE, DUTY_BEGIN);
+  }else if(angle<0){
+    // Turn Right
+    digitalWrite(RIGHT_REVERSE, HIGH);
+    analogWrite(PWM_MOVE, DUTY_BEGIN);
+  }
+}
+
+void moveStop(){
+  analogWrite(PWM_MOVE, 0);
+  delay(50);
+  digitalWrite(LEFT_REVERSE, LOW);
+  digitalWrite(RIGHT_REVERSE, LOW);
+}
+
+//=======================================================
+// Subscriber Callback Function
+void velCallback( const geometry_msgs::Twist& vel){
+  moveStop();
+  float vel_x = vel.linear.x; // Moving Forward Speed
+  float ang_z = vel.angular.z; // Angle to Turn
+  if (vel_x!=0){
+    moveForward();
+  }else if (ang_z!=0){
+    moveTurn(ang_z);
+  }
+}
+
+//=======================================================
+// Main Setup
 void setup() {
+  // Setup Pins
+  pinMode(LEFT_HALL_IN, INPUT);
+  pinMode(RIGHT_HALL_IN, INPUT);
+  pinMode(PWM_MOVE, OUTPUT);
+  pinMode(LEFT_REVERSE, OUTPUT);
+  pinMode(RIGHT_REVERSE, OUTPUT);
+  moveStop();
+
+  // ROS
+  robot.initNode();
+  robot.subscribe(sub_cmd_vel);
+  robot.advertise(pub_odom);
+
 }
 
 void loop() {
+  // SpinOnce Check for instructions
+  robot.spinOnce();
+
+  // Check Hall pins of both wheels for Rising Edge
+  pollHallPins();
+  
+  // Publish Odometry
+  robot.publish(&pose);
 }
+
+
+void pollHallPins(){
+  // Left wheel polling
+  left_hall_val = analogRead(LEFT_HALL_IN);
+  //Serial.println(left_hall_val);
+  if (!left_freq_done){
+    if (left_hall_val >= LEFT_HALL_THRESH) {
+      if (left_wasLowLevel == true) {
+        left_wasLowLevel = false;  // Reset edge detector
+
+        // Odometry Update
+        updateOdom(1); 
+      }
+    }else {
+      left_wasLowLevel = true;
+    }
+  }
+
+  // Right Wheel Polling
+  right_hall_val = analogRead(RIGHT_HALL_IN);
+  //Serial.println(right_hall_val);
+  if (right_hall_val >= RIGHT_HALL_THRESH) {
+    if (right_wasLowLevel == true) {
+      right_wasLowLevel = false;  // Reset edge detector
+
+      // Odometry Update
+      updateOdom(-1);
+    }
+  }else {
+    right_wasLowLevel = true;
+  }
+}
+
+// Function turn param takes +1 or -1 depending on left wheel or right wheel poll
+void updateOdom(int turn) {
+  if (turn>0){
+    leftHallCount++;
+  }else{
+    rightHallCount++;
+  }
+
+  // Serial.print("Left Hall Count: ");
+  // Serial.println(LeftHallCount);
+  float prev_theta = theta;
+  // Change in bearing = (90*L*pi)/(pi*HalfWidth*180) = 0.115267
+  theta = theta + turn*0.1153; // in radian
+  
+  // Serial.print("Bearing: ");
+  // Serial.println(new_bearing);
+  
+  x = x + HalfWidth*(cos(prev_theta) - cos(theta));
+  
+  // Serial.print("x coordinate: ");
+  // Serial.println(x);
+  
+  y = y + HalfWidth*(sin(theta) - sin(prev_theta));
+  
+  // Serial.print("y coordinate: ");
+  // Serial.println(y);
+  bearing = new_bearing;
+}
+
+
