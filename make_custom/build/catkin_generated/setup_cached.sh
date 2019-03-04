f15ed278e9cad752c355fe75458b71bfcb0698df
imandsy <iman.ds.yahya@gmail.com>
2019-03-04 22:52:22 +0000

    Add files via upload

---
+++ make_custom/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/iman/make_custom/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/iman/make_custom/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/iman/make_custom/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/iman/make_custom/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/iman/make_custom/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/iman/make_custom/src:$ROS_PACKAGE_PATH"
