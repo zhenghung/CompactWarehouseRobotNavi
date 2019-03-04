c17f9591924c8a853f95150594929ccbad785be5
imandsy <iman.ds.yahya@gmail.com>
2019-03-04 23:00:29 +0000

    Add files via upload

---
+++ make_custom/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/iman/make_custom/src
+# Build directory: /home/iman/make_custom/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(imu_read)
