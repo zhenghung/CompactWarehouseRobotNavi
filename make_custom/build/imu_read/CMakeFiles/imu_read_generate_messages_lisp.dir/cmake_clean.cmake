4dbed90e550c0eca0a2f1b0e187aab1038c30029
imandsy <iman.ds.yahya@gmail.com>
2019-03-04 23:03:02 +0000

    Add files via upload

---
+++ make_custom/build/imu_read/CMakeFiles/imu_read_generate_messages_lisp.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imu_read_generate_messages_lisp"
+  "/home/iman/make_custom/devel/share/common-lisp/ros/imu_read/msg/imu_read.lisp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/imu_read_generate_messages_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
