4bd6e387902cc68f605325c710967db80ade380c
imandsy <iman.ds.yahya@gmail.com>
2019-03-04 23:00:02 +0000

    Delete CTestTestfile.cmake

--- make_custom/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/iman/make_custom/src/imu_read
-# Build directory: /home/iman/make_custom/build/imu_read
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
