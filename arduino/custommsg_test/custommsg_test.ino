e1f9ddeb4f05891dc806d9c952cfaacbc6f77431
Zheng Hung Chuah <zheng_hung@hotmail.com>
2019-03-11 23:38:58 +0000

    Fixed pubsub.py now properly republishes to Imu topic

--- arduino/custommsg_test/custommsg_test.ino
+++ arduino/custommsg_test/custommsg_test.ino
@@ -17,6 +17,15 @@ void publishMsg(){
   custom_msg.y = 1/1000;
   custom_msg.theta = 1.5;
 
+  custom_msg.gx = 1;
+  custom_msg.gy = 1;
+  custom_msg.gz = 1;
+
+  custom_msg.ax = 2;
+  custom_msg.ay = 2;
+  custom_msg.az = 2;
+
+
   pub_custom.publish(&custom_msg);
 
 }
--- python/RobotMover/pubsub.py
+++ python/RobotMover/pubsub.py
@@ -21,8 +21,11 @@ def listener():
 
 def msgCallback(msg):
 	x,y,t = msg.x, msg.y, msg.theta
+        gx,gy,gz = msg.gx, msg.gy, msg.gz
+        ax,ay,az = msg.ax, msg.ay, msg.az
+        
 	odom_publish(x,y,t)
-        #tf_publish(x,y,t)
+        imu_publish(gx,gy,gz,ax,ay,az)
         rospy.loginfo(rospy.get_name()+"(%.3f)"%(x))
 
 def odom_publish(x,y,t):
@@ -51,18 +54,18 @@ def odom_publish(x,y,t):
 
 	odom.publish(odomMsg)   
 
-def imu_publish(x,y,t): 	
-	imu = rospy.Publisher("/Imu", Imu, queue_size=1)
+def imu_publish(gx,gy,gz,ax,ay,az):	
+        imu = rospy.Publisher("/Imu", Imu, queue_size=1)
         imuMsg = Imu()
-	
+
         imuMsg.header.stamp = rospy.get_rostime()
-	imuMsg.orientation_covariance[0] = -1;
-	imuMsg.angular_velocity.x = math.radians(gx);
-	imuMsg.angular_velocity.y = math.radians(gy);
-	imuMsg.angular_velocity.z = math.radians(gz);
-	imuMsg.linear_acceleration.x = ax*9.81;
-	imuMsg.linear_acceleration.y = ay*9.81;
-	imuMsg.linear_acceleration.z = az*9.81;
+        imuMsg.orientation_covariance[0] = -1
+        imuMsg.angular_velocity.x = math.radians(gx)
+        imuMsg.angular_velocity.y = math.radians(gy)
+        imuMsg.angular_velocity.z = math.radians(gz)
+        imuMsg.linear_acceleration.x = ax*9.81
+        imuMsg.linear_acceleration.y = ay*9.81
+        imuMsg.linear_acceleration.z = az*9.81
 
         imu.publish(imuMsg)      
 
